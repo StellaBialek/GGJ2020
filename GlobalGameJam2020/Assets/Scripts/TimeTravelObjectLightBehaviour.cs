ae3d5b485c09d41e4a2d8f38ab2cb2b517ac932f
Stella <bialek.stella@googlemail.com>
2020-02-01 16:09:41 +0100

    Implemented Depended Time Travel Object Behaviour

---
+++ GlobalGameJam2020/Assets/Scripts/TimeTravelObjectLightBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeTravelObjectLightBehaviour : TimeTravelObjectBehaviour
+{
+    public float Smooth = 2;
+    private Light lightsource;
+
+    public void Start()
+    {
+        lightsource = GetComponent<Light>();
+    }
+
+    public void Update()
+    {
+        lightsource.range = Mathf.Lerp(lightsource.range, Parent.IsLocked ? 3 : 0, Smooth * Time.deltaTime); 
+    }
+}
---
+++ GlobalGameJam2020/Assets/Scripts/TimeTravelObjectScaleBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeTravelObjectScaleBehaviour : TimeTravelObjectBehaviour
+{
+    public Vector3 Present = Vector3.one;
+    public Vector3 Past = Vector3.one;
+
+    void Update()
+    {
+        UpdateAffectionLevel();
+
+        transform.localScale = Vector3.Lerp(Present, Past, AffectionLevel);
+    }
+}
