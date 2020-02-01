8976a9c7682ff84bd6e9f309d251bec2a9d3d958
maxkrumm <39380018+maxkrumm@users.noreply.github.com>
2020-02-02 00:50:50 +0100

    Sound Update Helper

--- GlobalGameJam2020/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ GlobalGameJam2020/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -21,8 +21,6 @@ namespace AK
         static const AkUniqueID FOOTSTEPS = 2385628198U;
         static const AkUniqueID MUSIC = 3991942870U;
         static const AkUniqueID OBJ_LIGHT_01 = 4270401061U;
-        static const AkUniqueID OBJ_LIGHT_02 = 4270401062U;
-        static const AkUniqueID OBJ_LIGHT_03 = 4270401063U;
         static const AkUniqueID SFX_LOCK_FAIL = 1108173695U;
         static const AkUniqueID SFX_LOCK_FAR = 3674212506U;
         static const AkUniqueID SFX_LOCK_MUSIC = 231259868U;
