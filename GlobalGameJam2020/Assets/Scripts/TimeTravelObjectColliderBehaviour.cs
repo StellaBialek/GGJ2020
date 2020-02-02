58a699a7ec51677ccddcdd21214de10149ba6ab0
aayph <mmehlitz@gmail.com>
2020-02-02 04:24:04 +0100

    Progress on Events

---
+++ GlobalGameJam2020/Assets/Scripts/TimeTravelObjectColliderBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeTravelObjectColliderBehaviour : TimeTravelObjectBehaviour
+{
+    public bool Present;
+    public bool Past;
+
+    private Collider collider;
+
+
+    public void Start()
+    {
+        collider = GetComponent<Collider>();
+    }
+
+    void Update()
+    {
+        UpdateAffectionLevel();
+        if (AffectionLevel == 1)
+            collider.enabled = Past;
+        else
+            collider.enabled = Present;
+    }
+}
---
+++ GlobalGameJam2020/Assets/Scripts/TimeTravelObjectRotationBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeTravelObjectRotationBehaviour : TimeTravelObjectBehaviour
+{
+    public Vector3 Present = Vector3.one;
+    public Vector3 Past = Vector3.one;
+
+    void Update()
+    {
+        UpdateAffectionLevel();
+
+        transform.localRotation = Quaternion.Euler(Vector3.Lerp(Present, Past, AffectionLevel));
+    }
+}
+
